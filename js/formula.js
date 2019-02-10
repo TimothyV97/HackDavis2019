a4191b9e0d9159cc47360bb09dca98675639f39d
Enea Kllomollari <eneakllomollari@gmail.com>
2019-02-09 17:04:59 -0800

    empty input fix

--- js/formula.js
+++ js/formula.js
@@ -1,24 +1,22 @@
 function clickFunction() {
   var source = (document.getElementById('source').value);
   var dest = (document.getElementById('destination').value);
-  var car  = (document.getElementById('carType').value);
-  console.log(car);
-  var carMap = new Map();
 
-  var apiQuerry = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=';
-
-  const proxyurl = "https://cors-anywhere.herokuapp.com/";
-  var url = proxyurl + apiQuerry + source + '&destinations=' + dest + '&key=AIzaSyA5A-5qiyE0LJgG7_Ns5U2jZ422hvX4sGg';
-  var dist;
-  $.getJSON(url, function (data) {
-    if (data.rows.length > 1 || data.rows.elements.status == "NOT_FOUND") {
-      console.log("error");
-    }
-    else {
-      dist = console.log(data.rows[0].elements[0].distance.text);
-    }
-  });
-
+  if (source.length != 0 && dest.length != 0) {
+    var apiQuerry = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=';
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    var url = proxyurl + apiQuerry + source + '&destinations=' + dest + '&key=AIzaSyA5A-5qiyE0LJgG7_Ns5U2jZ422hvX4sGg';
+    
+    var dist;
+    $.getJSON(url, function (data) {
+      if (data.rows.length == 1) {
+        dist = (data.rows[0].elements[0].distance.text);
+      }
+      console.log(dist);
+    });
+  }else {
+    console.log("error");
+  }
 }
 
 function addCars() {
