d892736c9b2fb9e402fd911e29c32ed41d3cb06e
Steven <steventat@csus.edu>
2019-02-09 19:58:44 -0800

    Switched getJson to ajax

--- js/formula.js
+++ js/formula.js
@@ -1,23 +1,28 @@
 function clickFunction() {
   var source = (document.getElementById('source').value);
   var dest = (document.getElementById('destination').value);
-  var carType = (document.getElementById('carType').value);
+  var carMileage = (document.getElementById('carType').value);
   //console.log(carType);
   if (source.length != 0 && dest.length != 0) {
     var apiQuerry = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=';
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
     var url = proxyurl + apiQuerry + source + '&destinations=' + dest + '&key=AIzaSyA5A-5qiyE0LJgG7_Ns5U2jZ422hvX4sGg';
-    
+
     var dist;
-    $.getJSON(url, function (data) {
-      if (data.rows.length == 1) {
-        dist = parseFloat(data.rows[0].elements[0].distance.text);
+    $.ajax( {
+      url: url,
+      type: 'GET',
+      success: function (data) {
+        if (data.rows.length == 1) {
+          dist = parseFloat(data.rows[0].elements[0].distance.text);
+        }
+        console.log(dist*carMileage);
+
+        //Alters empty placeholder div to contain result
+        (document.getElementById("result")).innerHTML = dist*carMileage;
       }
-      //console.log(data.rows[0].elements[0].distance.text);
-      
-      console.log(dist * carType);
     });
-  }else {
+  } else {
     console.log("error");
   }
 }
