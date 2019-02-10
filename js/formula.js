363c9f46ee27060e6ab400f08abc7a6a35781fd9
Enea Kllomollari <eneakllomollari@gmail.com>
2019-02-09 16:13:29 -0800

    Working formula.js and api call

--- js/formula.js
+++ js/formula.js
@@ -1,3 +1,21 @@
 function clickFunction() {
-  document.write(document.getElementById('source').value);
+  var source = (document.getElementById('source').value);
+  var dest = (document.getElementById('destination').value);
+  var car  = (document.getElementById('carType').value);
+  var carMap = new Map();
+
+  var apiQuerry = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=';
+
+  const proxyurl = "https://cors-anywhere.herokuapp.com/";
+  var url = proxyurl + apiQuerry + source + '&destinations=' + dest + '&key=AIzaSyA5A-5qiyE0LJgG7_Ns5U2jZ422hvX4sGg';
+  var dist;
+  $.getJSON(url, function (data) {
+    if (data.rows.length > 1 || data.rows.elements.status == "NOT_FOUND") {
+      console.log("error");
+    }
+    else {
+      dist = console.log(data.rows[0].elements[0].distance.text);
+    }
+  });
+
 }
