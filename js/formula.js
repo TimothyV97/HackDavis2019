c2679168d79b268c3dc98d18b3746dca3c6d85a4
Enea Kllomollari <eneakllomollari@gmail.com>
2019-02-09 20:53:18 -0800

    Add current location variable

--- js/formula.js
+++ js/formula.js
@@ -29,12 +29,7 @@ function clickFunction() {
         if (data.rows.length == 1) {
           dist = parseFloat(data.rows[0].elements[0].distance.text.replace(/,/g, ''));
         }
-        //console.log(dist)
-        console.log(data);
-        console.log(carMileage);
-        console.log(dist);
-        //console.log(dist*carMileage);
-
+   
         //Alters placeholder div to contain result
         //Calculates to 
         (document.getElementById("result")).innerHTML = (dist*carMileage/1000).toFixed(2);
